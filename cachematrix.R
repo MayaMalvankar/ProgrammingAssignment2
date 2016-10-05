74319bff6ff52c2c603dd04a73c8e60e400f961b
Maya Malvankar <mayamalvankar@gmail.com>
2016-10-05 10:43:06 -0400

    added test code and comments to cahchematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
-# makeCacheMatrix creates matrix that can cache its inverse
-# cacheSolve computes inverse of matrix created by makeCacheMatrix either by picking it from cache if  
-# has been calculated else calculates it 
+## This file contains following two functions
+## makeCacheMatrix creates matrix that can cache its inverse
+## cacheSolve computes inverse of matrix created by makeCacheMatrix either by picking it from cache if  
+## has been calculated else calculates it 
 
-## Write a short comment describing this function
+
 #makeCacheMatrix creates a list with functions for
 #Setting value of matrix
 #Getting value of matrix
@@ -18,8 +17,11 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     invmat <<- NULL
   }
+  
   get <- function() x
+  
   setinverse <- function(inverse) invmat <<- inverse
+  
   getinverse <- function() invmat
   
   list(set = set, get = get,
@@ -28,9 +30,10 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
-## Write a short comment describing this function
+
+## cacheSolve return a matrix that is the inverse of 'x' from cache or calcuulates it if there's nothing in cache
 #Make sure that the matrix passed is invertible matrix
-## Return a matrix that is the inverse of 'x'
+
 cacheSolve <- function(x, ...) {
   
   
@@ -45,5 +48,23 @@ cacheSolve <- function(x, ...) {
   datamat <- x$get()
   invmat <- solve(datamat, ...)
   x$setinverse(invmat)
+  
   invmat
 }
+
+## Tested function execution using following 
+## > mat <- matrix(c(1,-1,1,-1,2,1,-1,3,4), 3,3) ## Create a matrix that can produce an inverse
+## > chkcache = makeCacheMatrix(mat) 
+## > cacheSolve(chkcache) ## First execution does not have cached inverse
+## [,1] [,2] [,3]
+## [1,]    5    3   -1
+## [2,]    7    5   -2
+## [3,]   -3   -2    1
+## > cacheSolve(chkcache) ## Second execution provides cached inverse 
+## getting cached data
+## [,1] [,2] [,3]
+## [1,]    5    3   -1
+## [2,]    7    5   -2
+## [3,]   -3   -2    1
+
+
